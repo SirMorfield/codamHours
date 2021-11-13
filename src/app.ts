a625754882d32dd98b907ba538742c936e6b8c69
SirMorfield <joppe.koers@gmail.com>
2021-11-13 16:00:28 +0100

    express and ejs init

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import express from "express"
+import path from "path"
+const app = express()
+
+app.set("views", path.join(__dirname, "../views"));
+app.set('viewengine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('index.ejs');
+})
+
+app.listen(8080, () => {
+	console.log('started')
+})
