df9a8606472be1751d814e39b8563ed29323e1e2
SirMorfield <joppe.koers@gmail.com>
2021-11-15 13:18:41 +0000

    very simple auth proof of concept

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import express from "express"
 import path from "path"
+import cookieSession from 'cookie-session'
 const app = express()
 
-app.set("views", path.join(__dirname, "../views"));
-app.set('viewengine', 'ejs');
+// const FileStore = sessionFileStore(session)
+import { authenticate, provider, passport } from './authentication'
 
-app.get('/', (req, res) => {
+
+app.set("views", path.join(__dirname, "../views"))
+app.set('viewengine', 'ejs')
+
+app.use(cookieSession({
+	maxAge: 24 * 60 * 60 * 1000,
+	keys: ['ddgasdgsagasassg3']
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get(`/auth/${provider}/`, passport.authenticate(provider, { scope: ['profile'] }))
+app.get(`/auth/${provider}/callback`,
+	passport.authenticate(provider, {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	}))
+
+app.get('/', authenticate, (req, res) => {
 	res.render('index.ejs');
 })
 
---
+++ src/authentication.ts
@@ -0,0 +1,71 @@
+import passport from 'passport'
+import { OAuth2Strategy } from 'passport-oauth'
+import fs from 'fs'
+import { v4 as uuid } from 'uuid'
+
+
+export function authenticate(req, res, next) {
+	if (!req.user) {
+		res.redirect('/auth/provider');
+	} else {
+		next();
+	}
+}
+
+interface User {
+	id: string,
+}
+const users: any[] = [
+
+]
+
+type URL = string
+
+interface Secrets {
+	clientUID: string,
+	clientSecret: string,
+	callbackURL: URL,
+	authorizationURL: URL,
+	tokenURL: URL
+}
+const secrets = JSON.parse(fs.readFileSync('./secrets.json').toString()) as Secrets
+export const provider = '42'
+
+passport.serializeUser((user, done) => {
+	console.log('serialize', user)
+	done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+	console.log('deserialize', id)
+	const user = users.find((user) => user.id === id)
+	done(null, user);
+})
+
+passport.use(provider, new OAuth2Strategy({
+	authorizationURL: secrets.authorizationURL,
+	tokenURL: secrets.tokenURL,
+	clientID: secrets.clientUID,
+	clientSecret: secrets.clientSecret,
+	callbackURL: secrets.callbackURL,
+	// passReqToCallback: true
+},
+	(accessToken, refreshToken, profile, done) => { // fires when user clicked allow
+		console.log('allow', accessToken, refreshToken, profile)
+		let user = users.find((user) => user.id === refreshToken)
+		if (!user) {
+			users.push({ id: refreshToken })
+			user = users.find((user) => user.id === refreshToken)
+		}
+		done(null, user);
+	}
+))
+
+export { passport }
+
+
+// router.get(`/${provider}/callback`,
+// 	passport.authenticate(provider), (req, res) => {
+// 		console.log(users)
+// 		res.send('logged in')
+// 	})
