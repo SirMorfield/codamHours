c623fee1379ff70666e0939ccf2fc76138e8411a
Marius van Wijk <mvan-wij@student.codam.nl>
2021-12-16 20:02:40 +0100

    Get weeks correctly

--- src/db/dateUtils.ts
+++ src/db/dateUtils.ts
@@ -16,22 +16,21 @@ export function formatDate(date: Date, hoursMinutes: boolean = true): Time.Date 
 }
 
 export function getWeekAndYear(date: Date): { year: number, week: number } {
-	let d = new Date(date)
-	d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()))
-	d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7)) // make sunday's day number 7
-	const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1))
-	// @ts-ignore
-	const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
-	return { year: d.getUTCFullYear(), week: weekNo };
+	const year = date.getFullYear()
+	const day = new Date(year, 0, 1).getDay()
+	const firstMonday = day == 1 ? day : 9 - day
+	const s = Math.floor((date.valueOf() - new Date(year, 0, firstMonday).valueOf()) / (7 * 24 * 60 * 60 * 1000)) + 1
+	if (s == 0)
+		return getWeekAndYear(new Date(year - 1, 11, 31))
+	return { year: year, week: s }
 }
 
-// weeknumber starts from 1
+// weeknumber starts from 1, weeks start on monday
 export function getWeekRange(year: number, week: number): { start: Time.Date, end: Time.Date } {
-	const d = new Date("Jan 01, " + year + " 01:00:00")
-	const firstDay = new Date(year, 0, 1).getDay();
-	const w = d.getTime() - (3600000 * 24 * (firstDay - 1)) + 604800000 * (week - 1)
+	const d = new Date(year, 0, week * 7)
+	const w = d.valueOf() - d.getDay() * 24 * 60 * 60 * 1000
 	const start = new Date(w)
-	const end = new Date(w + 518400000)
+	const end = new Date(w + 6 * 24 * 60 * 60 * 1000)
 	return {
 		start: formatDate(start, false),
 		end: formatDate(end, false),
