65a781e52f4ba1f1bd7032b1242dc7c665892cff
SirMorfield <joppe.koers@gmail.com>
2022-02-10 01:06:27 +0000

    error checking

--- src/db/getMailsSpreadSheet.ts
+++ src/db/getMailsSpreadSheet.ts
@@ -2,7 +2,7 @@ import { google, sheets_v4 } from 'googleapis'
 import { env } from '../env'
 import { Mail, MailID } from '../types'
 
-//date	subject	body	url	from	message id	id
+//date, subject, body, url, from, message id, id
 async function getSpreadsheet(client): Promise<string[][]> {
 	return new Promise((resolve) => {
 		const sheets = google.sheets({ version: 'v4', auth: client })
@@ -15,9 +15,8 @@ async function getSpreadsheet(client): Promise<string[][]> {
 				console.log(err)
 				resolve([])
 			}
-			const rows = res?.data.values
-			console.log(rows)
-			resolve(rows)
+			const rows = res?.data?.values
+			resolve(rows ? rows : [])
 		})
 	})
 }
@@ -28,12 +27,18 @@ export async function getMails(isInDb?: (id: MailID) => Promise<boolean>, maxRes
 	if (!client)
 		client = await auth.getClient()
 	const sheet: string[][] = await getSpreadsheet(client)
-	const mails = sheet.map(line => ({
-		id: line[6]!,
-		content: line[2]!,
-		from: line[4]!,
-		subject: line[1]!,
-		d: new Date(line[0]!),
-	}))
+	const mails: Mail[] = []
+	for (const line of sheet) {
+		try {
+			const m = {
+				id: line[6]!,
+				content: line[2]!,
+				from: line[4]!,
+				subject: line[1]!,
+				d: new Date(line[0]!),
+			}
+			mails.push(m)
+		} catch (e) { }
+	}
 	return mails
 }
